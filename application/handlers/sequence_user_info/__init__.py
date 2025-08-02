1cecc0828f895a68a434d82d61840e8ddfe84053
pnovikov <novikovpa@yandex.ru>
2025-08-02 11:40:18 +0300

    check autofix

--- application/handlers/sequence_user_info/__init__.py
+++ application/handlers/sequence_user_info/__init__.py
@@ -1,4 +1,5 @@
 from .user_info_router import sequence_user_info_router
 from .user_info_sequence_definition import user_info_sequence
 
-__all__ = ["sequence_user_info_router", "user_info_sequence"]
+__all__ = ["sequence_user_info_router",
+           "user_info_sequence"]
