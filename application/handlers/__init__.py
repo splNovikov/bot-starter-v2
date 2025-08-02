b0aea28d89ad2c20707a07dd0d2ee70176495993
pnovikov <novikovpa@yandex.ru>
2025-08-02 10:02:38 +0300

    fixes order

--- application/handlers/__init__.py
+++ application/handlers/__init__.py
@@ -2,5 +2,4 @@ from .registry_init import initialize_registry
 from .router import main_router
 from .sequence_user_info import user_info_sequence
 
-__all__ = ["initialize_registry",
-           "main_router", "user_info_sequence"]
+__all__ = ["initialize_registry", "main_router", "user_info_sequence"]
