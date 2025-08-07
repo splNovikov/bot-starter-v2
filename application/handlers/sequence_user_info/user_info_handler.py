f6a3a3a63415f6dfc066c675a93e862407245ce7
pnovikov <novikovpa@yandex.ru>
2025-08-07 11:47:22 +0300

    code formatting

--- application/handlers/sequence_user_info/user_info_handler.py
+++ application/handlers/sequence_user_info/user_info_handler.py
@@ -8,10 +8,13 @@ logger = get_logger()
 
 async def handle_user_info(message: Message):
     sequence_initiation_service = get_sequence_initiation_service()
-    
-    success, error_message = await sequence_initiation_service.initiate_user_info_sequence(
+
+    (
+        success,
+        error_message,
+    ) = await sequence_initiation_service.initiate_user_info_sequence(
         message, message.from_user
     )
-    
+
     if not success:
         await message.answer(error_message)
